82ad180e3fd7d73fa1fc4f8ba36e516aa9140e30
Utkarsh Raj <86511235+RaUtkarsh@users.noreply.github.com>
2022-10-14 13:34:35 +0530

    Create BestTimeToBuyAndSellStock.cpp

---
+++ BestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size() == 0) return 0;
+        int max = 0, min = prices[0];
+        for(int i = 0; i < prices.size(); i++){
+            if(prices[i] < min) min = prices[i];
+
+            if((prices[i] - min) > max) max = prices[i] - min;
+        }
+        return max;
+    }
+};
